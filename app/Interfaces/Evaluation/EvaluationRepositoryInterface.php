200c7cd104b9921ab62b54ff3b9b69edf2b626c4
MatheusSodre <matheus.sodree@gmail.com>
2023-05-09 16:31:56 -0300

    Micro_gateway

--- app/Interfaces/Evaluation/EvaluationRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Interfaces\Evaluation;
-
- interface EvaluationRepositoryInterface
- {
-    public function getEvaluationCompany(string $company);
- }
-
- ?>
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@
 use App\Http\Controllers\Api\Evaluation\EvaluationController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/evaluations/{company}',[EvaluationController::class,'index']);
 Route::post('/evaluations/{company}',[EvaluationController::class,'store']);
 
