174ddae2dc0105b230f501bc3d3ce45f071e3d55
MatheusSodre <matheus.sodree@gmail.com>
2023-06-14 09:57:01 -0300

    Implements crud user

--- app/Services/User/UserService.php
+++ app/Services/User/UserService.php
@@ -11,19 +11,49 @@ class UserService
     public function __construct( DefaultResponse $defaultResponse)
     {
         $this->defaultResponse = $defaultResponse;
-        $this->url  = config('microservices.available.micro_auth.url') . '/api/user';
+        $this->url  = config('microservices.available.micro_auth.url') . '/api';
         $this->http = Http::acceptJson()
                             ->withHeaders([
                                 'Authorization' => request()->header('Authorization')
                             ]);
     }
-
+    
+    public function allUser(array $params=[])
+    {
+        $response = $this->http
+                            ->get($this->url . '/user',$params);
+        return response()->json(json_decode($response->body()), $response->status());                    
+    }
+    public function createUser(array $params=[])
+    {
+        $response = $this->http
+                            ->post($this->url . '/user',$params);
+        return response()->json(json_decode($response->body()), $response->status());                    
+    }
+    public function getUser($identify)
+    {
+        $response = $this->http
+                            ->get($this->url . "/user/{$identify}");
+        return response()->json(json_decode($response->body()), $response->status());                    
+    }
+    public function updateUser(string $identify, array $params = [])
+    {
+        $response = $this->http
+                            ->put($this->url . "/user/{$identify}" , $params);
+        return response()->json(json_decode($response->body()), $response->status());                    
+    }
+    public function deleteUser(string $identify, array $params = [])
+    {
+        $response = $this->http
+                            ->delete($this->url . "/user/{$identify}");
+        return response()->json(json_decode($response->body()), $response->status());                    
+    }
     
     public function addPermissionUser(array $params=[])
     {
         $response = $this->http
-                              ->post($this->url . '/permission' ,$params);
-        return json_decode($response->body());
+                              ->post($this->url . '/user/permission' ,$params);
+        return response()->json(json_decode($response->body()), $response->status());                    
     }
 }
 ?>
--- routes/api.php
+++ routes/api.php
@@ -11,17 +11,26 @@ use App\Http\Controllers\Api\User\{
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/companies', [CompanyController::class,'index']);
-Route::post('/companies',[CompanyController::class,'store']);
-Route::get('/companies/{company}',[CompanyController::class,'show']);
-Route::put('/companies/{company}',[CompanyController::class,'update']);
-Route::delete('/companies/{company}',[CompanyController::class,'destroy']);
+// Route::get('/companies', [CompanyController::class,'index']);
+// Route::post('/companies',[CompanyController::class,'store']);
+// Route::get('/companies/{company}',[CompanyController::class,'show']);
+// Route::put('/companies/{company}',[CompanyController::class,'update']);
+// Route::delete('/companies/{company}',[CompanyController::class,'destroy']);
+
 
 Route::post('/register',[RegisterController::class, 'store']);
 Route::post('/auth',    [AuthController::class, 'authUser']);
 Route::post('/logout',  [AuthController::class, 'logout']);
 Route::get('/me',       [AuthController::class, 'me']);
+
 Route::post('/user/permission',[PermissionUserController::class, 'addPermissionUser']);
+Route::apiResource('/user',UserController::class);
+
+Route::apiResource('/companies',CompanyController::class);
+
+
+
+
 
 
 
